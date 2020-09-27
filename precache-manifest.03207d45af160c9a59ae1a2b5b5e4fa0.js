1fc4ba536a7a812bcc746663c61d67fd6df8b4c6
naveen-06 <70632802+naveen-06@users.noreply.github.com>
2020-09-26 22:22:46 -0400

    Add files via upload

---
+++ precache-manifest.03207d45af160c9a59ae1a2b5b5e4fa0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c3bb1fa3d2766cc44871f5b0ffd2472",
+    "url": "/pizza-hut/index.html"
+  },
+  {
+    "revision": "a7101c630bbb8d55ae1c",
+    "url": "/pizza-hut/static/css/main.f1458f1e.chunk.css"
+  },
+  {
+    "revision": "2a0567fa520ce8231763",
+    "url": "/pizza-hut/static/js/2.590d3ea3.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/pizza-hut/static/js/2.590d3ea3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7101c630bbb8d55ae1c",
+    "url": "/pizza-hut/static/js/main.e9027c1a.chunk.js"
+  },
+  {
+    "revision": "74e9ba80550ae395fbcb",
+    "url": "/pizza-hut/static/js/runtime-main.e1de52d3.js"
+  }
+]);
